10e7d36c82fcaf08db9606b552911feb0a755d46
Heba Bou KaedBey <156496150+hebabkb@users.noreply.github.com>
2025-04-01 23:57:53 -0400

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -16,6 +16,42 @@ def cap_outliers(df, cols, percentile=0.99):
             df.loc[:, col] = df[col].clip(upper=upper_limit)
     return df
 
+def remove_correlated_variables(X_train, X_test, vif_thresh=5, corr_thresh=0.7):
+    #Removes highly correlated and high-VIF variables.
+    numerical_features = X_train.select_dtypes(include=[np.number]).columns.tolist()
+    important_features = ['fico', 'amtfinanced_1req', 'ltv_1req', 'pti_1req']
+    features_to_drop = []
+    
+    vif_data = pd.DataFrame()
+    vif_data["feature"] = numerical_features
+    vif_data["VIF"] = [variance_inflation_factor(X_train[numerical_features].values, i)
+                        for i in range(len(numerical_features))]
+    
+    high_vif_features = vif_data[vif_data["VIF"] > vif_thresh]["feature"].tolist()
+    high_vif_features = [feat for feat in high_vif_features if feat not in important_features]
+    features_to_drop.extend(high_vif_features)
+    
+    corr_matrix = X_train.drop(columns=features_to_drop).corr().abs()
+    for i in range(len(corr_matrix.columns)):
+        for j in range(i):
+            if corr_matrix.iloc[i, j] > corr_thresh:
+                colname_i = corr_matrix.columns[i]
+                colname_j = corr_matrix.columns[j]
+                if colname_i in important_features:
+                    features_to_drop.append(colname_j)
+                elif colname_j in important_features:
+                    features_to_drop.append(colname_i)
+                else:
+                    mean_corr_i = corr_matrix[colname_i].mean()
+                    mean_corr_j = corr_matrix[colname_j].mean()
+                    features_to_drop.append(colname_i if mean_corr_i > mean_corr_j else colname_j)
+    
+    features_to_drop = list(set(features_to_drop))
+    X_train_reduced = X_train.drop(columns=features_to_drop)
+    X_test_reduced = X_test.drop(columns=features_to_drop)
+    
+    return X_train_reduced, X_test_reduced
+
 def preprocess_data(train_df, test_df):
     #Preprocesses the data including handling missing values, outlier capping, and feature transformations.
     missing_percent_train = ((train_df.isnull().sum() / len(train_df)) * 100).round(2)
@@ -68,44 +104,11 @@ def preprocess_data(train_df, test_df):
     feature_names = preprocessor.get_feature_names_out()
     X_train = pd.DataFrame(X_train, columns=feature_names)
     X_test = pd.DataFrame(X_test, columns=feature_names)
+
+    X_train, X_test = remove_correlated_variables(X_train, X_test)
     
     return X_train, y_train, X_test, y_test
 
-def remove_correlated_variables(X_train, X_test, vif_thresh=5, corr_thresh=0.7):
-    #Removes highly correlated and high-VIF variables.
-    numerical_features = X_train.select_dtypes(include=[np.number]).columns.tolist()
-    important_features = ['fico', 'amtfinanced_1req', 'ltv_1req', 'pti_1req']
-    features_to_drop = []
-    
-    vif_data = pd.DataFrame()
-    vif_data["feature"] = numerical_features
-    vif_data["VIF"] = [variance_inflation_factor(X_train[numerical_features].values, i)
-                        for i in range(len(numerical_features))]
-    
-    high_vif_features = vif_data[vif_data["VIF"] > vif_thresh]["feature"].tolist()
-    high_vif_features = [feat for feat in high_vif_features if feat not in important_features]
-    features_to_drop.extend(high_vif_features)
-    
-    corr_matrix = X_train.drop(columns=features_to_drop).corr().abs()
-    for i in range(len(corr_matrix.columns)):
-        for j in range(i):
-            if corr_matrix.iloc[i, j] > corr_thresh:
-                colname_i = corr_matrix.columns[i]
-                colname_j = corr_matrix.columns[j]
-                if colname_i in important_features:
-                    features_to_drop.append(colname_j)
-                elif colname_j in important_features:
-                    features_to_drop.append(colname_i)
-                else:
-                    mean_corr_i = corr_matrix[colname_i].mean()
-                    mean_corr_j = corr_matrix[colname_j].mean()
-                    features_to_drop.append(colname_i if mean_corr_i > mean_corr_j else colname_j)
-    
-    features_to_drop = list(set(features_to_drop))
-    X_train_reduced = X_train.drop(columns=features_to_drop)
-    X_test_reduced = X_test.drop(columns=features_to_drop)
-    
-    return X_train_reduced, X_test_reduced
 
 if __name__ == "__main__":
     train_data_path = "/Users/heba/Desktop/Erdos/Training Dataset A_R-384891_Candidate Attach #1_PresSE_SRF #1142.csv"
